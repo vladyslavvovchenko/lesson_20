a06ffe41aa5b9a4e1dd80a3991c1d7a0f49e878f
vladyslavvovchenko <93626934+vladyslavvovchenko@users.noreply.github.com>
2026-02-08 22:48:41 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -2,6 +2,5 @@ const burger = document.querySelector('.icon-menu');
 const menuBody = document.querySelector('.header__menu');
 
 burger.addEventListener('click', () => {
-    document.documentElement.classList.toggle('open-menu'); // гамбургер + меню
-    document.body.classList.toggle('menu-open');            // блокуємо скрол
+    document.documentElement.classList.toggle('open-menu'); 
 });
